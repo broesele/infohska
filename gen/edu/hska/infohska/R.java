c155e69b7385e40b55a6f4227b47f430afeb21ac
Broesele <broesel@antinone.net>
2013-01-07 16:32:07 +0100

    2

--- gen/edu/hska/infohska/R.java
+++ gen/edu/hska/infohska/R.java
@@ -11,7 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int hska_logo_ohne_text=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon_aktuelles=0x7f020002;
+        public static final int icon_bachelor_studiengang=0x7f020003;
+        public static final int icon_master_studiengang=0x7f020004;
+        public static final int icon_personen=0x7f020005;
     }
     public static final class id {
         public static final int item_detail=0x7f060002;
